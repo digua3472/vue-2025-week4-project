2c79251a8d76af1f8b7d6c5d94d9cc4b5fe19879
digua <ah5925zq@gmail.com>
2025-08-25 18:04:49 +0800

    update

--- vue-2025-week4-project/vite.config.js
+++ vue-2025-week4-project/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue-2025-week4-project/',
+  base: process.env.NODE_ENV === 'production' ? '/vue-2025-week4-project/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
